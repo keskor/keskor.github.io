dfcd11fce9f36667f5fbb3991c9a503ba97a24fb
keskor <35984529+keskor@users.noreply.github.com>
2018-02-14 10:34:23 -0700

    Toggles for Data Veiwing

--- script/body.js
+++ script/body.js
@@ -1,21 +1,26 @@
 require([
       "esri/Map",
 	  "esri/layers/MapImageLayer",
+	  //"esri/layers/FeatureLayer",
       "esri/views/MapView",
+	  "dojo/dom",  // require dojo/dom for getting the DOM element
+	  "dojo/on",   // require dojo/on for listening to events on the DOM
       "dojo/domReady!"
-    ], function(Map, MapImageLayer, MapView) {
+    ], function(Map, /*FeatureLayer,*/ MapImageLayer, MapView, dom, on) {
 
+	//Create the Map
       var map = new Map({
         basemap: "streets"
       });
 	  
 	  var utah_dem_layer = new MapImageLayer({url: "http://geoserver2.byu.edu/arcgis/rest/services/Valor/Elevations/MapServer"});
 	  
-	  var utah_shp_layer = new MapImageLayer({url: "http://geoserver2.byu.edu/arcgis/rest/services/Valor/SpeedLimit/MapServer"});
+	  var speedlimitlayer = new MapImageLayer({url: "http://geoserver2.byu.edu/arcgis/rest/services/Valor/SpeedLimit/MapServer"});
 	  
 	  map.layers.add(utah_dem_layer);
-	  map.layers.add(utah_shp_layer);
+	  map.layers.add(speedlimitlayer);
 
+	  //Create the Map View
       var view = new MapView({
         map: map,
 		container: "viewDiv",
@@ -23,4 +28,48 @@ require([
         center: [-111.1, 39.1] // longitude, latitude
       });
 
+	  /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+	  //The Popup
+	  var popup = { // autocasts as new PopupTemplate()
+        title: "Information about the road: {Name}",
+        content: "<p>This road is in, <b>{ADDRSYS_L}%</b> and has a zip code of {ZIPCODE_L}.</p>" +
+          "<ul><li>The speed limit is {SPEED_LMT} mph</li>",
+        fieldInfos: [{
+          fieldName: "SPEED_LMT",
+          format: {
+            digitSeparator: true, // Use a comma separator for large numbers
+            places: 0 // Sets the number of decimal places to 0 and rounds up
+          }
+        }]
+      };
+	  
+	  	// Reference the popupTemplate instance in the
+		// popupTemplate property of FeatureLayer
+	  var featureLayer = new FeatureLayer({
+        url: "http://geoserver2.byu.edu/arcgis/rest/services/Valor/SpeedLimit/MapServer/0",
+        outFields: ["*"],
+        popupTemplate: popup
+		});
+		map.add(featureLayer);
+	  
+	  ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+	  	// Create a variable referencing the checkbox node for Utah DEM
+      var demLyrToggle = dom.byId("dem");
+	    
+		// Listen to the onchange event for the checkbox
+		on(demLyrToggle, "change", function(){
+		// When the checkbox is checked (true), set the layer's visibility to true
+		utah_dem_layer.visible = demLayerToggle.checked;
+		});
+		
+		// Create a variable referencing the checkbox node for Utah SpeedLimit
+	  var speedlimiLyrToggle = dom.byId("speedlimit");
+	  
+		// Listen to the onchange event for the checkbox
+		on(speedlimiLyrToggle, "change", function(){
+		// When the checkbox is checked (true), set the layer's visibility to true
+		speedlimitlayer.visible = speedlimiLyrToggle.checked;
+		});
+
+	  
     });
